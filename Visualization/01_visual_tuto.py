efba7d238c8896fd06c291a0c6f4dd5b95575c4e
tientn182819 <tien.tn182819@sis.hust.edu.vn>
2023-06-17 16:02:24 +0700

    Add test

---
+++ Visualization/01_visual_tuto.py
@@ -0,0 +1,9 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+fig = plt.figure()    
+#[left, bottom, width, height]
+ax = plt.axes([0.2, 0.2, 1, 1])
+ax = plt.axes([0, 1.2, 1, 1])
+
